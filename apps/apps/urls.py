834b807c31fe84a6d85e5cb0c6186195a3bef20c
Edward Bridges <github@eqbridges.com>
2019-01-21 05:55:27 -0500

    app_name is a required param in django 2.0. https://stackoverflow.com/a/48609128/87408

--- apps/apps/urls.py
+++ apps/apps/urls.py
@@ -1,4 +1,5 @@
-from django.conf.urls import include, url
+from django.conf.urls import url
+from django.urls import path, include
 from django.contrib import admin
 
 import theme.views
@@ -10,10 +11,10 @@ import books_pc_multi_view2.urls
 
 urlpatterns = [
     url(r'^$', theme.views.home, name='home'),
-    url(r'^books_simple/', include(books_simple.urls, namespace='books_simple')),
-    url(r'^books_pc_formset/', include(books_pc_formset.urls, namespace='books_pc_formset')),
-    url(r'^books_pc_formset2/', include(books_pc_formset2.urls, namespace='books_pc_formset2')),
-    url(r'^books_pc_multi_view/', include(books_pc_multi_view.urls, namespace='books_pc_multi_view')),
-    url(r'^books_pc_multi_view2/', include(books_pc_multi_view2.urls, namespace='books_pc_multi_view2')),
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^books_simple/', include((books_simple.urls, 'books_simple'), namespace='books_simple')),
+    url(r'^books_pc_formset/', include((books_pc_formset.urls, 'books_pc_formset'), namespace='books_pc_formset')),
+    url(r'^books_pc_formset2/', include((books_pc_formset2.urls, 'books_pc_formset2'), namespace='books_pc_formset2')),
+    url(r'^books_pc_multi_view/', include((books_pc_multi_view.urls, 'books_pc_multi_view'), namespace='books_pc_multi_view')),
+    url(r'^books_pc_multi_view2/', include((books_pc_multi_view2.urls, 'books_pc_multi_view2'), namespace='books_pc_multi_view2')),
+    path('admin/', admin.site.urls),
 ]
